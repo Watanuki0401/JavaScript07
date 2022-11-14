f93744a1fe3b7ec5007d79202136319c7b79515b
Watanuki0401 <114453407+Watanuki0401@users.noreply.github.com>
2022-11-14 12:34:46 +0900

    Add files via upload

---
+++ js/script01.js
@@ -0,0 +1,5 @@
+console.log("Hello")
+
+x = 2;
+y = 3;
+console.log(x+y);
---
+++ js/script02.js
@@ -0,0 +1,41 @@
+var collback = "Hello from script02";
+document.write(collback);
+document.write("<br>");
+
+var num1 = 3;
+var num2 = 4.8;
+
+document.write(num1);
+document.write("<br>");
+document.write(num2);
+document.write("<br>");
+
+
+var area = 3*7;
+document.write(area);
+document.write("<br>");
+
+var str = 'Hello' + 'world.';
+document.write(str);
+document.write("<br>");
+
+var comOp = 3 > 7;
+document.write(comOp);
+document.write("<br>");
+
+var logicOp = (3 < 7) && (1 < 2);
+document.write(logicOp);
+document.write("<br>");
+
+var operEx1 = 1 + "10";
+document.write(operEx1);
+document.write("<br>");
+
+var operEx2 = 2 * "10";
+document.write(operEx2);
+document.write("<br>");
+
+
+let f1 = document.getElementsByClassName('f1');
+let f2 = document.getElementsByClassName('f2');
+document.write('f1'+'f2')
